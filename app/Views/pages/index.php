f357ee96f35e0b9e54482eb885cdd9a715bd0204
joelAIbarraDino <joel.ibarra@dinozign.com>
2025-03-17 14:09:53 -0600

    se añade ¿seccion por que nosotros?

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -21,6 +21,41 @@
 
 </section>
 
+<section class="why-us">
+
+    <h2 class="why-us__title">¿Por qué nosotros?</h2>
+    
+    <p class="why-us__text">
+        Somos una institución constituida por profesionales experimentados 
+        en actividades economicas necesarias al desarrollo de empresas y al ser humano
+    </p>
+
+    <div class="why-us__grid">
+        <div class="razon">
+            <img class="razon__img" src="/assets/images/case-img-1.jpg" alt="imagen de razon 1">
+            <p class="razon__sub" >Razón-01</p>
+            <h3 class="razon__title">Maestros altamente especializados</h3>
+        </div>
+        <div class="razon">
+            <img class="razon__img" src="/assets/images/case-img-2.jpg" alt="imagen de razon 1">
+            <p class="razon__sub" >Razón-02</p>
+            <h3 class="razon__title">+2500 estudiantes exitosos</h3>
+        </div>
+        <div class="razon">
+            <img class="razon__img" src="/assets/images/case-img-3.jpg" alt="imagen de razon 1">
+            <p class="razon__sub" >Razón-03</p>
+            <h3 class="razon__title">Multiples modalidades en nuestros cursos</h3>
+        </div>
+
+        <div class="razon">
+            <img class="razon__img" src="/assets/images/case-img-4.jpg" alt="imagen de razon 1">
+            <p class="razon__sub" >Razón-04</p>
+            <h3 class="razon__title">Mentorias personalizadas</h3>
+        </div>
+    </div>
+
+</section>
+
 <?php include_once __DIR__.'/../components/footer.php'; ?>
 
 
