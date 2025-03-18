f36066cf01f68fa03bfdebfdee3105e888065e86
joelAIbarraDino <joel.ibarra@dinozign.com>
2025-03-18 01:20:39 -0600

    footer terminado

---
+++ app/Views/components/footer.php
@@ -0,0 +1,70 @@
+<footer class="footer">
+    <div class="KTA">
+        
+        <img  class="KTA-logo" src="/assets/images/logoKTA.jpg" alt="logo de KTA">
+
+        <p class="KTA-texto">
+            Ayudando al inmigrante en su integración total 
+            y exitosa en los Estados Unidos, 
+            ayudamos a las personas a alcanzar sus metas 
+            y perseguir sus sueños.
+        </p>
+
+        <ul class="KTA-social">
+            <li class="KTA-social__item"><a href="/"><i class='bx bxl-facebook-circle'></i></a></li>
+            <li class="KTA-social__item"><a href="/"><i class='bx bxl-instagram'></i></a></li>
+            <li class="KTA-social__item"><a href="/"><i class='bx bxl-youtube'></i></a></li>
+        </ul>
+    </div>
+
+    <div class="menu">
+        <h3 class="menu__title">Menú</h3>
+        
+        <nav class="menu__nav">
+            <ul class="nav">
+                <li class="nav__link"><a href="/">Home</a></li>
+                <li class="nav__link"><a href="/curses">Cursos</a></li>
+                <li class="nav__link"><a href="/about">Sobre nosotros</a></li>
+                <li class="nav__link"><a href="/memberships">Membresias</a></li>
+            </ul>
+        </nav>
+
+    </div>
+
+    <div class="contact">
+
+        <h3 class="contact__title">Contactanos</h3>
+
+        <div class="information">
+            <div class="information__icon">
+                <i class='bx bxs-map bx-tada' ></i>
+            </div>
+            <div class="information__data">
+                <a class="information__data-name" href="">KTA location</a>
+                <p class="information__data-label">Brandon, Florida</p>
+            </div>
+        </div>
+
+        <div class="information">
+            <div class="information__icon">
+                <i class='bx bx-envelope bx-tada' ></i>
+            </div>
+            <div class="information__data">
+                <a class="information__data-name" href="">Contacto</a>
+                <p class="information__data-label">soporte@ktainstitute.com</p>
+            </div>
+        </div>
+
+        <div class="information">
+            <div class="information__icon">
+                <i class='bx bxs-phone-call bx-tada' ></i>
+            </div>
+            <div class="information__data">
+                <a class="information__data-name" href="">Telefono</a>
+                <p class="information__data-label">(888)5921822</p>
+            </div>
+        </div>
+
+    </div>
+
+</footer>
