85440fbe0ce01c2c0649b391c860cfeb5212cc62
joelAIbarraDino <joel.ibarra@dinozign.com>
2025-04-03 03:09:42 -0600

    tabla de inscripciones creadas

--- app/Views/admin/enrollment/index.php
+++ app/Views/admin/enrollment/index.php
@@ -4,74 +4,96 @@
     <div class="main__container">
 
         <div class="top-main">
-            <h1 class="main__title">Inscripciones registradas</h1>
+            <h1 class="top-main__title">Inscripciones registradas</h1>
         </div>
 
-        <div class="table-4 show-2">
+        <div class="table-8 show-3">
 
-            <div class="header">
-                <p class="title-header" >Titulo 1</p>
-                <p class="title-header hidden">Titulo 2</p>
-                <p class="title-header" >titulo 3</p>
-                <p class="title-header hidden">titulo 4</p>
-            </div>
+            <?php if(count($enrollment) > 0): ?>
 
-            <div class="row">
-
-                <div class="">
-                    <p class="info-title" >Titulo 1</p>
+                <div class="header">
+                    <p class="title-header" >Caratula</p>
+                    <p class="title-header">Nombre</p>
+                    <p class="title-header hidden" >Creado el </p>
+                    <p class="title-header hidden">Privacidad</p>
+                    <p class="title-header hidden">Inscritos</p>
+                    <p class="title-header hidden">Acceso a contenido</p>
+                    <p class="title-header hidden">Maestro</p>
+                    <p class="title-header">Acciones</p>
                 </div>
+                <?php foreach($enrollment as $enroll):?>
+                    <div class="row">
 
-                <div class="hidden">
-                    <p class="info-title">Titulo 2</p>
+                        <div class="cell">
+                            <div class="col-main">
+                                <a class="col-main-thumbnail" href="/curso/<?=$course->id_course?>">
+                                    <img src="/assets/thumbnails/<?=$course->thumbnail?>" alt="caratula de curso">
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="cell">
+                            <div class="col-main-2">
+                                <a href="/admin/curso/edit/<?=$course->id_course?>" class="col-main-2-title"><?=$course->name?></a>
+                                <p class="col-main-2-category"><?=$course->category?></p>
+
+                                <?php if(isset($course->discount)):?>
+                                    <div class="col-main-2-prices">
+                                        <div class="col-main-2-prices-price disc">$<?=$course->price?> USD</div>
+                                        <div class="col-main-2-prices-disc">$<?=$course->price * (1 - ($course->discount/100))?> USD</div>
+                                    </div>
+                                <?php else:?>    
+                                    <div class="col-main-2-prices">
+                                        <div class="col-main-2-prices-price">$<?=$course->price?> USD</div>
+                                    </div>
+                                <?php endif;?>
+                            </div>
+                        </div>
+                        
+                        <div class="cell hidden">
+                            <div class="col-info">
+                                <p class="col-info-date"><?=$course->created_at?></p>
+                            </div>
+                        </div>
+                        
+                        <div class="cell hidden">
+                            <div class="col-info">
+                                <p class="col-info-date"><?=$course->privacy ?></p>
+                            </div>
+                        </div>
+                        
+                        <div class="cell hidden">
+                            <div class="col-info">
+                                <a href="/maestro/10" class="col-info-date--link"><?=$course->enrollment?></a>
+                            </div>
+                        </div>
+                        
+                        <div class="cell hidden">
+                            <div class="col-info">
+                                <p class="col-info-date"><?=$course->max_months_enroll ?> meses</p>
+                            </div>
+                        </div>
+                        
+                        <div class="cell hidden">
+                            <div class="col-info">
+                                <a href="/admin/maestro/<?=$course->id_teacher ?>" class="col-info-date--link"><?=$course->teacher ?></a>
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <div class="col-action">
+                                <button class="col-action-link delete-course" data-id="<?=$course->id_course?>">Eliminar</button>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach;?>
+
+            <?php else: ?>
+                <div class="empty">
+                    <p class="text">No hay inscripciones registradas</p>
                 </div>
+            <?php endif; ?>
 
-                <div class="hidden">
-                    <p class="info-title" >titulo 3</p>
-                </div>
-
-                <div class="">
-                    <p class="info-title">titulo 4</p>
-                </div>
-            </div>
-
-            <div class="row">
-
-                <div class="">
-                    <p class="info-title" >Titulo 1</p>
-                </div>
-
-                <div class="hidden">
-                    <p class="info-title">Titulo 2</p>
-                </div>
-
-                <div class="hidden">
-                    <p class="info-title" >titulo 3</p>
-                </div>
-
-                <div class="">
-                    <p class="info-title">titulo 4</p>
-                </div>
-            </div>
-
-            <div class="row">
-
-                <div class="">
-                    <p class="info-title" >Titulo 1</p>
-                </div>
-
-                <div class="hidden">
-                    <p class="info-title">Titulo 2</p>
-                </div>
-
-                <div class="hidden">
-                    <p class="info-title" >titulo 3</p>
-                </div>
-
-                <div class="">
-                    <p class="info-title">titulo 4</p>
-                </div>
-            </div>
         </div>
 
     </div>
